4b0cbf76c0baf461266aebfa021648e87389a309
Louis Houghton <Louis@Midgar.local>
2015-03-21 18:37:17 +0000

    It seems RequestBody was needed afterall...

--- code/SmartIrrigatorWateringNode/src/main/java/com/louishoughton/irrigator/web/IrrigationController.java
+++ code/SmartIrrigatorWateringNode/src/main/java/com/louishoughton/irrigator/web/IrrigationController.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +27,7 @@ public class IrrigationController {
     }
 
     @RequestMapping(method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE) 
-    public IrrigationResponse irrigate(IrrigationRequest request) {
+    public IrrigationResponse irrigate(@RequestBody IrrigationRequest request) {
         try {
             valve.openFor(request.getSeconds());
         } catch (IrrigationValveException e) {
