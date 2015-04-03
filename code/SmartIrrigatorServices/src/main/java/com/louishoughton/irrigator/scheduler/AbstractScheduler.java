52d6015145979e445d67b4c87baa4b32395a20e4
Louis Houghton <Louis@Midgar.local>
2015-04-03 17:51:04 +0100

    Making the scheduler a bit more extensible. The AbstractScheduler has the main logic. Subclasses just provide the times at which to schedule the morning and evening executions.

---
+++ code/SmartIrrigatorServices/src/main/java/com/louishoughton/irrigator/scheduler/AbstractScheduler.java
@@ -0,0 +1,32 @@
+package com.louishoughton.irrigator.scheduler;
+
+import java.time.LocalDateTime;
+
+import org.springframework.scheduling.annotation.Scheduled;
+
+import com.louishoughton.irrigator.job.JobFactory;
+
+public abstract class AbstractScheduler implements Scheduler {
+
+    public static final String MIDNIGHT_CRON_STRING = "0 0 * * *";
+    
+    private final JobFactory jobFactory;
+    private final TaskScheduler taskScheduler;
+    
+    public AbstractScheduler(TaskScheduler taskScheduler, JobFactory jobFactory) {
+        this.taskScheduler = taskScheduler;
+        this.jobFactory = jobFactory;
+    }
+    
+    @Override
+    @Scheduled(cron = MIDNIGHT_CRON_STRING)
+    public void scheduleExecutions() {
+        taskScheduler.schedule(jobFactory.newJob(), getMorningExecutionDate());
+        taskScheduler.schedule(jobFactory.newJob(), getEveningExecutionDate());
+    }
+
+    protected abstract LocalDateTime getEveningExecutionDate();
+
+    protected abstract LocalDateTime getMorningExecutionDate();
+
+}
--- code/SmartIrrigatorServices/src/main/java/com/louishoughton/irrigator/scheduler/BasicScheduler.java
+++ code/SmartIrrigatorServices/src/main/java/com/louishoughton/irrigator/scheduler/BasicScheduler.java
@@ -1,27 +1,28 @@
 package com.louishoughton.irrigator.scheduler;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.louishoughton.irrigator.job.JobFactory;
 
-public class BasicScheduler implements Scheduler {
+@Component
+public class BasicScheduler extends AbstractScheduler {
 
-    private JobFactory jobFactory;
-    private TaskScheduler taskScheduler;
-    
-    /** 
-     * Executes the provided Runnable with a basic schedule using
-     * the provided TaskScheduler.
-     * @param execution The Runnable to be executed
-     * @param taskScheduler The TaskScheduler with which to execute the Runnable
-     */
+    @Autowired
     public BasicScheduler(TaskScheduler taskScheduler, JobFactory jobFactory) {
-        this.taskScheduler = taskScheduler;
-        this.jobFactory = jobFactory;
+        super(taskScheduler, jobFactory);
     }
-    
+
     @Override
-    public void scheduleExecutions() {
-        taskScheduler.schedule(jobFactory.newJob(), new BasicMorningExecutionDate().getDate());
-        taskScheduler.schedule(jobFactory.newJob(), new BasicEveningExecutionDate().getDate());
+    protected LocalDateTime getEveningExecutionDate() {
+        return new BasicEveningExecutionDate().getDate();
+    }
+
+    @Override
+    protected LocalDateTime getMorningExecutionDate() {
+        return new BasicMorningExecutionDate().getDate();
     }
 
 }
