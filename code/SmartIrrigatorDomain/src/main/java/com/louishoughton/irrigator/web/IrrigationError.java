01f351d9d84984f47eea72dde0da0b53941cc2f0
Louis Houghton <Louis@Midgar.local>
2015-04-12 14:47:39 +0100

    Forgot to actually check in the moved file....

---
+++ code/SmartIrrigatorDomain/src/main/java/com/louishoughton/irrigator/web/IrrigationError.java
@@ -0,0 +1,17 @@
+package com.louishoughton.irrigator.web;
+
+public class IrrigationError {
+
+    private final String message;
+
+    public IrrigationError(String message) {
+        super();
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    
+    
+}
