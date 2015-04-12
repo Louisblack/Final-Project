b475d5862a240dabac09ceb2ec8b606075f11a39
Louis Houghton <Louis@Midgar.local>
2015-04-12 14:54:47 +0100

    And the missing location one....

---
+++ code/SmartIrrigatorDomain/src/main/java/com/louishoughton/irrigator/forecast/LocationException.java
@@ -0,0 +1,11 @@
+package com.louishoughton.irrigator.forecast;
+
+public class LocationException extends Exception {
+
+
+    private static final long serialVersionUID = -6758061290088720196L;
+
+    public LocationException(Exception exception) {
+        super(exception);
+    }
+}
