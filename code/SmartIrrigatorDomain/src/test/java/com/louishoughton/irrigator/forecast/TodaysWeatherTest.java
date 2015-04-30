0550a84fbd114f77baa49848597d5e7029b0e241
Louis Houghton <louis@louishoughton.com>
2015-04-30 21:17:52 +0100

    Fixing Checkstyle issue

--- code/SmartIrrigatorDomain/src/test/java/com/louishoughton/irrigator/forecast/TodaysWeatherTest.java
+++ code/SmartIrrigatorDomain/src/test/java/com/louishoughton/irrigator/forecast/TodaysWeatherTest.java
@@ -3,7 +3,9 @@ package com.louishoughton.irrigator.forecast;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class TodaysWeatherTest {
 
